7a4ce1df75d07ba7bd057ed83339bfa7fc66f767
Robério <roberiomg@gmail.com>
2022-12-12 11:05:03 -0300

    first commit

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express'
+import multer from "multer"
+
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController"
+import { importCategoryController } from '../modules/cars/useCases/importCategory'
+import { listCategoriesController } from '../modules/cars/useCases/listCategories'
+
+
+
+
+const categoriesRoutes = Router()
+
+const upload = multer({
+    dest: "./tmp"
+})
+
+const createCategoryController = new CreateCategoryController()
+
+categoriesRoutes.post("/", createCategoryController.handle)
+
+categoriesRoutes.get("/", (request, response) => {
+    return listCategoriesController.handle(request, response)
+})
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    return importCategoryController.handle(request, response)
+})
+
+export { categoriesRoutes }
