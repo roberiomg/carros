07c8a04b86103467555c0abb7ef13cd947141cee
Robério <roberiomg@gmail.com>
2022-12-13 10:52:13 -0300

    Create Users

---
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -0,0 +1,25 @@
+import { IUserRepository } from "../repositories/IUserRepository";
+import { inject, injectable } from "tsyringe";
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+
+
+@injectable()
+class CreateUserUseCase {
+    constructor(
+        @inject("UserRepository")
+        private userRepository: IUserRepository) {}
+
+        async execute({name, username, email, password, driver_license}: ICreateUserDTO): Promise<void> {
+
+            await this.userRepository.create({
+                name,
+                username,
+                email,
+                password,
+                driver_license
+            })
+        }
+    
+}
+
+export { CreateUserUseCase }
