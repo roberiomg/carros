18e20dcc284a3639c95d4c49dcb9fcc7be3b781d
Robério <roberiomg@gmail.com>
2022-12-13 11:20:35 -0300

    Remover pasta node_modules

--- src/modules/accounts/useCases/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -6,7 +6,7 @@ import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 @injectable()
 class CreateUserUseCase {
     constructor(
-        @inject("UserRepository")
+        @inject("UsersRepository")
         private userRepository: IUserRepository) {}
 
         async execute({name, username, email, password, driver_license}: ICreateUserDTO): Promise<void> {
