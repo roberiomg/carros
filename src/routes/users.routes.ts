444db9d22fe0351d804d6731a3d6971a0f7ab33f
Robério <roberiomg@gmail.com>
2022-12-19 16:23:58 -0300

    Avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,24 @@
 import { Router } from "express"
-import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController"
+import multer from "multer"
 
-const userRoutes = Router()
+import uploadConfig from "../config/upload"
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated"
+import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController"
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController"
+
+const usersRoutes = Router()
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
 
 const createUserController = new CreateUserController()
+const updateUserAvatarController = new UpdateUserAvatarController()
 
-userRoutes.post("/", createUserController.handle)
+usersRoutes.post("/", createUserController.handle)
 
-export { userRoutes }
+usersRoutes.patch(
+    "/avatar",
+    ensureAuthenticated,
+    uploadAvatar.single("avatar"),
+    updateUserAvatarController.handle)
+
+export { usersRoutes }
