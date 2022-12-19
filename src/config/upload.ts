a50d4ee60ac459e7eff70c4c5ed078ea48130fc6
Robério <roberiomg@gmail.com>
2022-12-19 20:30:45 -0300

    Correção Avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,16 +3,17 @@ import multer from "multer";
 import { resolve } from "path";
 
 export default {
-
     upload(folder: string) {
-        storage: multer.diskStorage({
-            destination: resolve(__dirname, "..", "..", folder),
-            filename: (request, file, callback) => {
-                const fileHash = crypto.randomBytes(16).toString("hex")
-                const fileName = `${fileHash}-${file.originalname}`
+        return {
+            storage: multer.diskStorage({
+                destination: resolve(__dirname, "..", "..", folder),
+                filename: (request, file, callback) => {
+                    const fileHash = crypto.randomBytes(16).toString("hex")
+                    const fileName = `${fileHash}-${file.originalname}`
 
-                return callback(null, fileName)
-            }
-        })
+                    return callback(null, fileName)
+                }
+            })
+        }
     }
 }
