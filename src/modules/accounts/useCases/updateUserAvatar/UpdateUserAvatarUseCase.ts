917883dc44a3794ff5d31e45549926a094ceb3e7
Robério <roberiomg@gmail.com>
2022-12-20 09:39:55 -0300

    Remove avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,4 +1,7 @@
 import { inject, injectable } from "tsyringe";
+
+import { deleteFile } from "../../../../utils/file"
+
 import { IUserRepository } from "../../repositories/IUserRepository";
 
 
@@ -16,6 +19,10 @@ class UpdateUserAvatarUseCase {
     
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.userRepository.findById(user_id)
+
+        if(user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`)
+        }
 
         user.avatar = avatar_file
 
